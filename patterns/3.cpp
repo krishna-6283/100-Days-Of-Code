40f32e91aa73f4f9555e9a800d8cf89673a4fe1e
Gopal Krishna Shrivastava <122596081+krishna-6283@users.noreply.github.com>
2024-01-01 23:40:18 +0530

    Add files via upload

---
+++ patterns/3.cpp
@@ -0,0 +1,21 @@
+// n=3
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	int k =1;
+	 for (int i =1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			cout<<k<<" ";
+			k+=1;
+		}
+		cout<<endl;
+	 }
+	return 0;
+}
