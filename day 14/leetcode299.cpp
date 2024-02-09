1ded5a3ff3325b4108990eb7e55f81b34367d7b6
Gopal Krishna Shrivastava <122596081+krishna-6283@users.noreply.github.com>
2024-02-09 23:25:29 +0530

    Add files via upload

---
+++ day 14/leetcode299.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        unordered_map<char , int> mp1;
+        unordered_map<char , int> mp2;
+        int bulls =0;
+        int cows =0;
+        for(int i =0;i<secret.size();i++){
+            if(secret[i] == guess[i]){
+                bulls++;
+            }
+            else{
+                mp1[secret[i]]++;
+                mp2[guess[i]]++;
+            }
+            
+        }
+        for(auto m:mp2){
+                cows+=min(m.second,mp1[m.first]);
+            }
+        return to_string(bulls)+'A'+to_string(cows)+'B';
+
+
+    }
+};
